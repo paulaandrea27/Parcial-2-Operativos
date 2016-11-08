7efc485bf824b200828af05db175dcf51ffd48fe
paulaandrea27 <pau.andre27@gmail.com>
2016-11-07 21:47:55 -0500

    Update run_tests_with_coverage.sh

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e 
 
-. ~/.virtualenvs/myproject/bin/activate
+. ~/virtualenvs/myproject/bin/activate
 
 PYTHONPATH=. py.test --junitxml=python_tests_myproject.xml
 PYTHONPATH=. py.test --cov-report xml --cov=../myproject
